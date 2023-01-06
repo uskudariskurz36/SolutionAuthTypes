38c68fd44c19713bec3222f6eddb802e95f0ff4c
Murat Başeren <uskudar.iskur.z36@gmail.com>
2023-01-06 14:49:34 +0300

    Add project files.

---
+++ AppIndividualAuth/EmailSender.cs
@@ -0,0 +1,18 @@
+﻿
+namespace WebApp_IndividualAuth
+{
+    public class EmailSender : Microsoft.AspNetCore.Identity.UI.Services.IEmailSender
+    {
+        private readonly ISendGridHelper sendGridHelper;
+
+        public EmailSender(ISendGridHelper sendGridHelper)
+        {
+            this.sendGridHelper = sendGridHelper;
+        }
+
+        public async Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            await sendGridHelper.Execute(email, subject, htmlMessage);
+        }
+    }
+}
---
+++ AppIndividualAuth/SendGridHelper.cs
@@ -0,0 +1,39 @@
+﻿using SendGrid.Helpers.Mail;
+using SendGrid;
+using Microsoft.Extensions.Options;
+
+namespace WebApp_IndividualAuth
+{
+    public interface ISendGridHelper
+    {
+        Task Execute(string email, string subject, string htmlMessage);
+    }
+
+    public class SendGridHelper : ISendGridHelper
+    {
+        private readonly IConfiguration configuration;
+
+        public SendGridHelper(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        public async Task Execute(string email, string subject, string htmlMessage)
+        {
+            var apiKey = configuration.GetValue<string>("AppSettings:SendGridKey");
+            var client = new SendGridClient(apiKey);
+            var from = new EmailAddress("uskudar.iskur.z36@gmail.com", "Test Account");
+            var to = new EmailAddress(email);
+            //var plainTextContent = "and easy to do anywhere, even with C#";
+            var msg = MailHelper.CreateSingleEmail(from, to, subject, "", htmlMessage);
+            
+            msg.SetClickTracking(false, false);
+
+            var response = await client.SendEmailAsync(msg);
+
+            //_logger.LogInformation(response.IsSuccessStatusCode
+            //                   ? $"Email to {toEmail} queued successfully!"
+            //                   : $"Failure Email to {toEmail}");
+        }
+    }
+}
